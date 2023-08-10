7672e9b4d159e151d549d1e32e61414f3a4b5b58
SaLud2023 <sabrina.ludwig@capgemini.com>
2023-08-10 08:56:02 +0200

    Bug #1: Change sort direction

--- src/main/java/cmd/commands/dir/DirCommand.java
+++ src/main/java/cmd/commands/dir/DirCommand.java
@@ -81,9 +81,9 @@ public class DirCommand implements Runnable {
   private Comparator<String> getSortOrderAwareFileNameComparator(final String sortOrder) {
 
     if ("desc".equals(sortOrder)) {
-      return String::compareTo;
-    } else if ("asc".equals(sortOrder)) {
       return Comparator.reverseOrder();
+    } else if ("asc".equals(sortOrder)) {
+      return String::compareTo;
     } else {
       return (fileName1, fileName2) -> 0;
     }
